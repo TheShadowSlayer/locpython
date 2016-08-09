7e4d0238831432ce6823b8263333fb3ad074b907
TheShadowSlayer <loliveirachace@gmail.com>
2016-08-09 12:03:49 -0400

    Created pyfit.py

---
+++ pyfit.py
@@ -0,0 +1,16 @@
+test_steps = [32, 45, 75, 88, 92, 84, 79, 63, 42, 20, 32, 56, 62, 49, 43]
+steps = []
+for line in open("sleep.txt"):
+    step = line.split()[4]
+    steps.append(step)
+    if (step != ""):
+    	steps.append(step)
+
+for i in range(len(steps)):
+	x = int(steps[i]) / 100
+	x = int(x)
+	print(x*">")
+
+	if(range(len(steps)	) =< 150:
+		print: ":"
+	else:
